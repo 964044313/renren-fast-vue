67ea01385c7ed34ba03fa1878bcd7aff0ec17198
majiping <964044313@qq.com>
2023-01-20 15:54:29 +0800

    商品系统-属性分组

--- src/views/modules/common/category-cascader.vue
+++ src/views/modules/common/category-cascader.vue
@@ -1,12 +1,12 @@
 <template>
-<!--
-使用说明：
-1）、引入category-cascader.vue
-2）、语法：<category-cascader :catelogPath.sync="catelogPath"></category-cascader>
-    解释：
-      catelogPath：指定的值是cascader初始化需要显示的值，应该和父组件的catelogPath绑定;
-          由于有sync修饰符，所以cascader路径变化以后自动会修改父的catelogPath，这是结合子组件this.$emit("update:catelogPath",v);做的
-      -->
+  <!--
+  使用说明：
+  1）、引入category-cascader.vue
+  2）、语法：<category-cascader :catelogPath.sync="catelogPath"></category-cascader>
+      解释：
+        catelogPath：指定的值是cascader初始化需要显示的值，应该和父组件的catelogPath绑定;
+            由于有sync修饰符，所以cascader路径变化以后自动会修改父的catelogPath，这是结合子组件this.$emit("update:catelogPath",v);做的
+        -->
   <div>
     <el-cascader
       filterable
@@ -30,7 +30,7 @@ export default {
   props: {
     catelogPath: {
       type: Array,
-      default(){
+      default() {
         return [];
       }
     }
@@ -47,15 +47,12 @@ export default {
       paths: this.catelogPath
     };
   },
-  watch:{
-    // catelogPath(v){
-    //   this.paths = this.catelogPath;
-    // },
-    paths(v){
-      this.$emit("update:catelogPath",v);
+  watch: {
+    paths(v) {
+      this.$emit("update:catelogPath", v);
       //还可以使用pubsub-js进行传值
       // this.PubSub.publish("catPath",v);
-    }
+    },
   },
   //方法集合
   methods: {
@@ -63,15 +60,15 @@ export default {
       this.$http({
         url: this.$http.adornUrl("/product/category/tree"),
         method: "get"
-      }).then(({ data }) => {
+      }).then(({data}) => {
         this.categorys = data.data;
       });
-    }
+    },
   },
   //生命周期-创建完成（可以访问当前this实例）
   created() {
     this.getCategorys();
-  }
+  },
 };
 </script>
 <style scoped>
